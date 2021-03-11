518989a3600e9919c99e495b3dc7f2fdbe6d7f6d
alexisraelmtz <alex.israel.mtz@gmail.com>
2021-03-10 20:27:53 -0600

    setting up Main template and prep models + urls

--- NowGo_app/kanban_board/models.py
+++ NowGo_app/kanban_board/models.py
@@ -1,3 +1,36 @@
 from django.db import models
 
-# Create your models here.
+
+class users(models.Model):
+    # uid = models.
+    # username = models.
+    pass
+
+
+class home(models.Model):
+    # uid = models.
+    # board = models.
+    # title= models.
+    pass
+
+
+class board(models.Model):
+    # bid = models.
+    # column = models.
+    # title = models.
+    pass
+
+
+class column(models.Model):
+    # cid = models
+    # data = models
+    # date = models
+    pass
+
+
+class ticket(models.Model):
+    # tid =
+    # name =
+    # date =
+    # position =
+    pass
--- NowGo_app/kanban_board/views.py
+++ NowGo_app/kanban_board/views.py
@@ -1,3 +1,19 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 
-# Create your views here.
+# from .models import board, column, house, city
+
+
+def myTest(request):
+    return render(request, "index.html")
+
+
+def myBoard(request):
+    # Product.objects.all() / .filter() / .get() / .save()
+    # return HttpResponse("Hello, World!")
+    # items = Board.objects.all()
+    return render(request, "index.html", {"items": items})
+
+
+def myHome(request):
+    return HttpResponse("New View!\n\n Hello, World!!!")
