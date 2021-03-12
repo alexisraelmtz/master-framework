9acccf44a42dd9159c5539207c1535d71450085a
alexisraelmtz <alex.israel.mtz@gmail.com>
2021-03-11 22:40:58 -0600

    models debugged and migrated

--- NowGo_app/kanban_board/models.py
+++ NowGo_app/kanban_board/models.py
@@ -13,7 +13,10 @@ maxUsers = 100
 
 class user(models.Model):
     uid = models.IntegerField(
-        auto_increment=True, primary_key=True, unique=True, max_length=maxUsers
+        max_length=maxUsers,
+        # auto_increment=True,
+        primary_key=True,
+        unique=True,
     )
     username = models.CharField(max_length=25)
     email = models.EmailField(max_length=85)
@@ -22,41 +25,44 @@ class user(models.Model):
 
 class board(models.Model):
     bid = models.IntegerField(
-        auto_increment=True,
+        # auto_increment=True,
         primary_key=True,
         unique=True,
         max_length=maxBoards * maxUsers,
     )
     uid = models.IntegerField(max_length=maxUsers)
     title = models.CharField(max_length=225)
-    date = models.DateTimeField(datetime.now(tz=None))
+    date = models.DateTimeField(datetime.datetime.now(tz=None))
     style = models.IntegerField(max_length=100)
 
 
 class column(models.Model):
     cid = models.IntegerField(
-        auto_increment=True,
+        # auto_increment=True,
         primary_key=True,
         unique=True,
         max_length=maxColumns * maxBoards * maxUsers,
     )
     bid = models.IntegerField(max_length=maxBoards * maxUsers)
     name = models.CharField(max_length=125)
-    date = models.DateTimeField(datetime.now(tz=None))
+    date = models.DateTimeField(datetime.datetime.now(tz=None))
     position = models.IntegerField(max_length=600)
     style = models.IntegerField(max_length=100)
 
 
 class ticket(models.Model):
     tid = models.IntegerField(
-        auto_increment=True, primary_key=True, unique=True, max_length=maxTickets
+        # auto_increment=True,
+        primary_key=True,
+        unique=True,
+        max_length=maxTickets,
     )
     cid = models.IntegerField(max_length=maxColumns * maxBoards * maxUsers)
     name = models.CharField(max_length=255)
     text = models.TextField(max_length=750)
     priority = models.IntegerField(max_length=5)
     progress = models.IntegerField(max_length=100)
-    date = models.DateTimeField(datetime.now(tz=None))
+    date = models.DateTimeField(datetime.datetime.now(tz=None))
     position = models.IntegerField(max_length=200)
 
     # class home(models.Model):
